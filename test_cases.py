a213a0b4af363b3d4f947014604662a56f78e285
ASHRITHSHETTY123 <97445115+ASHRITHSHETTY123@users.noreply.github.com>
2023-02-27 22:59:21 +0530

    Update test_cases.py

--- test_cases.py
+++ test_cases.py
@@ -1,8 +1,8 @@
-import calcProg
+import myfile
 
 def test_add():
-    assert calcProg.add(3,5) == 8
+    assert myfile.add(3,5) == 8
 def test_subtract():
-    assert calcProg.subtract(5,3) == 2
+    assert myfile.subtract(5,3) == 2
 def test_add():
-    assert calcProg.multiply(3,5) == 15
+    assert myfile.multiply(3,5) == 15
